15a58840e8181683757ebfbf0f9d1b052c66bf6a
PensioneK <pensionek@baronegalasso.com>
2026-08-13 13:01:22 -0700

    Added computer and user game logic

--- Blackjack.py
+++ Blackjack.py
@@ -7,6 +7,27 @@ done = False
 userHand = []
 computerHand = []
 
+def userGameLoop(hand):
+    stay = False
+    while not stay:
+        take_hit = input("Type 'y' to get another card, type 'n' to pass:  ").lower()
+        if take_hit == 'y':
+            hand.append(random.choices(cards, k=1)[0])
+            if sum(hand) > 21:
+                return hand
+            else:
+                print(f"\tYour cards:  {hand}, current score:  {sum(hand)}")
+                print(f"\tComputer's first card:  {computerHand[1]}")
+        else:
+            stay = True
+    return hand
+
+def computerGameLoop(hand):
+    while sum(hand)<=17:
+        hand.append(random.choices(cards, k=1)[0])
+    
+    return hand
+
 while not done:
     play = input("Do you want to play a game of Blackjack? Type 'y' or 'n':  ").lower()
     if play == "n":
@@ -18,19 +39,19 @@ while not done:
     userHand = random.choices(cards, k=2)
     computerHand = random.choices(cards, k=2)
 
-    print(f"Your cards:  {userHand}, current score: {sum(userHand)}")
-    print(f"Computer's first card:  {computerHand[1]}")
+    print(f"\tYour cards:  {userHand}, current score: {sum(userHand)}")
 
-    if computerHand[1] >= 10 and (computerHand[0] + computerHand[1] == 21):
-        print("Lose, opponent has Blackjack")
+    if computerHand[1] >= 10 and (sum(computerHand) == 21):
+        print(f"\tComputer's final hand:  {computerHand}")
+        print("You loose, Computer has Blackjack")
         continue
+    else:
+        print(f"\tComputer's first card:  {computerHand[1]}")
+        userHand = userGameLoop(userHand)
+        computerHand = computerGameLoop(computerHand)
 
-    takeHit = input("Type 'y' to get another card, type 'n' to pass:  ").lower()
-    while takeHit == 'y':
-        userHand.append(random.choice(cards))
-        print(f"Your cards:  {userHand}, current score: {sum(userHand)}")
-        if sum(userHand) > 21:
-            
-
+        print(f"\tYour final cards:  {userHand}, final score:  {sum(userHand)}")
+        print(f"\tComputer's final hand:  {computerHand}, final score: {sum(computerHand)}")
+        
        
     
