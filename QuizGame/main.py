8876ae3872dfcdc5761dc1182b64fd913539163b
Eric Underwood <e.b.underwood@gmail.com>
2026-08-19 00:36:21 -0700

    Started adding functionality to QuizBrain class

--- QuizGame/main.py
+++ QuizGame/main.py
@@ -1,10 +1,12 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
 for question in question_data:
-    question_text = question['text']
-    question_answer = question['answer']
+    question_text = question["text"]
+    question_answer = question["answer"]
     question_bank.append(Question(question_text, question_answer))
 
+my_quiz_brain = QuizBrain(question_bank)
---
+++ QuizGame/quiz_brain.py
@@ -0,0 +1,12 @@
+class QuizBrain:
+    # Constructor
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.question_list = q_list
+
+    # Methods
+    def get_question(self):
+        current_question = self.question_list[self.question_number]
+        user_answer = input(
+            f"Q.{self.question_number + 1}:  {current_question}.  (True/False)?:  "
+        ).lower()
