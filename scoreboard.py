6ce1d3e260128360edd4fa2b9dbc18e67338c7e6
Eric Underwood <e.b.underwood@gmail.com>
2026-08-23 13:25:30 -0700

    Finished snake game and day 21 notes

---
+++ scoreboard.py
@@ -0,0 +1,36 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.penup()
+        self.goto(0, 280)
+        self.color("white")
+        self.write(
+            f"Score: {self.score}",
+            move=False,
+            align="center",
+            font=("Arial", 14, "bold"),
+        )
+        self.hideturtle()
+
+    def update_score(self):
+        self.score += 1
+        self.clear()
+        self.write(
+            f"Score: {self.score}",
+            move=False,
+            align="center",
+            font=("Arial", 14, "bold"),
+        )
+
+    def print_final_score(self):
+        self.home()
+        self.write(
+            "Game Over!",
+            move=False,
+            align="center",
+            font=("Arial", 26, "bold"),
+        )
--- snake.py
+++ snake.py
@@ -4,6 +4,8 @@ from turtle import Screen
 import random
 import time
 from snake_class import Snake
+from food import Food
+from scoreboard import Scoreboard
 
 # Set up the game window.
 game_window = Screen()
@@ -14,6 +16,8 @@ game_window.tracer(0)  # -- Smooths out snake animation
 
 # Set up our snake
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 # Set up snake movement
 game_window.listen()
@@ -25,10 +29,35 @@ game_window.onkey(key="d", fun=snake.right)
 
 # Move the snake
 game_is_on = True
+
 while game_is_on:
     game_window.update()  # -- Smooths out snake animation
     time.sleep(0.1)
     snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.update_score()
+
+    # Detect collision with wall
+    if (
+        snake.head.xcor() > 290
+        or snake.head.xcor() < -290
+        or snake.head.ycor() > 290
+        or snake.head.ycor() < -290
+    ):
+        game_is_on = False
+        scoreboard.print_final_score()
+
+    # Detect collision with tail
+    for segment in snake.snake:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.print_final_score()
 
 
 game_window.exitonclick()
