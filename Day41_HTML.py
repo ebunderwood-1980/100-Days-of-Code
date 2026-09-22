79b04beedeac738634efc46dc722ae72534b8846
PensioneK <pensionek@baronegalasso.com>
2026-09-22 11:37:04 -0700

    Day 41 - HTML <h>, <p>, <br>, and <hr>

---
+++ Day41_HTML.py
@@ -0,0 +1,13 @@
+# ----- How does the internet work -----
+#
+#  Client -->  Internet Service Provider --> Domain Name System
+#
+#  NSLooking.io shows exact ip address of a specific website.
+#
+#
+# ----- How do websites work -----
+#
+#  Websites contain HTML, CSS, and/or JS
+#  HTML - Structure of website.
+#  CSS -  Style of website.
+#  JS  -  Allows website to do things (behavior)
